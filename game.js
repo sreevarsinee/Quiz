f9c8bdb9f94bdcf738dee6d2c49ca71edc67ee32
sree varsinee <46200096+sreevarsinee@users.noreply.github.com>
2021-01-03 15:09:54 +0530

    Update game.js

--- game.js
+++ game.js
@@ -1,20 +1,21 @@
 const question = document.getElementById('question');
 const choices = Array.from(document.getElementsByClassName('choice-text'));
-const progressText = document.getElementById("progressText");
-const scoreText = document.getElementById("score");
-const progressBarFull = document.getElementById("progressBarFull");
-
+const progressText = document.getElementById('progressText');
+const scoreText = document.getElementById('score');
+const progressBarFull = document.getElementById('progressBarFull');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuesions = [];
+
 let questions = [];
 
 fetch(
     'https://opentdb.com/api.php?amount=10&category=18&difficulty=medium&type=multiple'
-    )
-   
+)
     .then((res) => {
         return res.json();
     })
@@ -38,11 +39,13 @@ fetch(
 
             return formattedQuestion;
         });
+
         startGame();
     })
     .catch((err) => {
         console.error(err);
     });
+
 //CONSTANTS
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 10;
@@ -52,18 +55,21 @@ startGame = () => {
     score = 0;
     availableQuesions = [...questions];
     getNewQuestion();
+    game.classList.remove('hidden');
+    loader.classList.add('hidden');
 };
 
 getNewQuestion = () => {
     if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score);
         //go to the end page
-        localStorage.setItem("mostRecentScore", score);
         return window.location.assign('G:/studies/projects/Quiz/end.html');
     }
     questionCounter++;
     progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
-  //Update the progress bar
-  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+    //Update the progress bar
+    progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
     const questionIndex = Math.floor(Math.random() * availableQuesions.length);
     currentQuestion = availableQuesions[questionIndex];
     question.innerText = currentQuestion.question;
@@ -84,22 +90,24 @@ choices.forEach((choice) => {
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset['number'];
-        const classToApply =
-      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
-      if (classToApply === "correct") {
-        incrementScore(CORRECT_BONUS);
-      }
-    selectedChoice.parentElement.classList.add(classToApply);
 
-    setTimeout(() => {
-      selectedChoice.parentElement.classList.remove(classToApply);
-      getNewQuestion();
-    }, 1000);
-        
+        const classToApply =
+            selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+
+        if (classToApply === 'correct') {
+            incrementScore(CORRECT_BONUS);
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
     });
 });
-incrementScore = num => {
+
+incrementScore = (num) => {
     score += num;
     scoreText.innerText = score;
-  };
-
+};
